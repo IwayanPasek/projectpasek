daa6b0da7ba2fd70496bee4666e4220b0442b179
I WAYAN PASEK GEDE SEGARA CAYMAN <45864852+IwayanPasek@users.noreply.github.com>
2025-08-01 19:19:01 +0800

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,20 +1,17 @@
 <?php
-// Menggunakan informasi koneksi ke Azure SQL Database
+// Informasi koneksi ke Azure SQL Database
 $serverName = "tcp:wayan.database.windows.net,1433";
 $database = "laundrydb";
 $username = "wayan";
 $password = "Jentung18";
 
 try {
-    // Membuat koneksi PDO baru
+    // Membuat koneksi PDO
     $conn = new PDO("sqlsrv:server=$serverName;Database=$database", $username, $password);
     // Mengatur mode error untuk menampilkan exception jika terjadi kesalahan
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // HAPUS BARIS INI DARI FILE ANDA: echo "Connected successfully!";
-
 } catch (PDOException $e) {
     // Menghentikan skrip dan menampilkan pesan error jika koneksi gagal
-    die("Koneksi gagal: " . $e->getMessage());
+    die("Koneksi ke database gagal: " . $e->getMessage());
 }
 ?>
