3df972ddf9ddf64d88a9e2ede02aa097cdca213b
I WAYAN PASEK GEDE SEGARA CAYMAN <45864852+IwayanPasek@users.noreply.github.com>
2025-08-01 19:20:31 +0800

    Update tandai_selesai.php

--- proses/tandai_selesai.php
+++ proses/tandai_selesai.php
@@ -1,18 +1,13 @@
 <?php
 include('../config/db.php');
 
-// Mengambil ID dari form
 $id = $_POST['id'];
 
 try {
-    // Menyiapkan query UPDATE dengan placeholder
     $sql = "UPDATE pelanggan SET status = 'Selesai' WHERE id = ?";
     $stmt = $conn->prepare($sql);
-    
-    // Menjalankan query dengan ID yang sesuai
     $stmt->execute([$id]);
 
-    // Mengalihkan kembali ke dashboard
     header('Location: ../dashboard.php');
 } catch (PDOException $e) {
     die("Error menandai selesai: " . $e->getMessage());
