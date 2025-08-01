44571a0ea6eaaceaf29aa50ad030023a2013198d
I WAYAN PASEK GEDE SEGARA CAYMAN <45864852+IwayanPasek@users.noreply.github.com>
2025-08-01 19:07:30 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
+<?php include('config/db.php'); ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Input Pelanggan</title>
+    <title>Tambah Pelanggan - Laundry</title>
 </head>
 <body>
-    <h2>Tambah Pelanggan Baru</h2>
-    <form action="proses/tambah_pelanggan.php" method="POST">
+    <h2>Form Tambah Pelanggan</h2>
+    <form action="proses/simpan_pelanggan.php" method="POST">
         <label>Nama:</label><br>
         <input type="text" name="nama" required><br>
-        <label>No WA:</label><br>
+        <label>No. WhatsApp:</label><br>
         <input type="text" name="no_wa" required><br>
         <label>Layanan:</label><br>
-        <input type="text" name="layanan" required><br><br>
+        <select name="layanan" required>
+            <option value="laundry komplit">Laundry Komplit</option>
+            <option value="hanya keringkan">Hanya Keringkan</option>
+            <option value="hanya setrika">Hanya Setrika</option>
+        </select><br><br>
         <button type="submit">Simpan</button>
     </form>
+    <br><a href="dashboard.php">Lihat Dashboard</a>
 </body>
 </html>
