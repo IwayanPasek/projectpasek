d54f2e3d0b0bdd2b782c28e316170aff0cfbf974
I WAYAN PASEK GEDE SEGARA CAYMAN <45864852+IwayanPasek@users.noreply.github.com>
2025-08-01 18:58:34 +0800

    Update simpan_pelanggan.php

--- proses/simpan_pelanggan.php
+++ proses/simpan_pelanggan.php
@@ -1,8 +1,14 @@
 <?php
 include('../config/db.php');
+
 $nama = $_POST['nama'];
 $no_wa = $_POST['no_wa'];
 $layanan = $_POST['layanan'];
-$conn->query("INSERT INTO pelanggan (nama, no_wa, layanan) VALUES ('$nama', '$no_wa', '$layanan')");
+
+$sql = "INSERT INTO pelanggan (nama, no_wa, layanan) VALUES (?, ?, ?)";
+$stmt = $conn->prepare($sql);
+$stmt->execute([$nama, $no_wa, $layanan]);
+
 header('Location: ../dashboard.php');
+exit;
 ?>
