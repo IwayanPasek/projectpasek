2c2881d684f2a81b9c30778b1f70585f612dd8ca
I WAYAN PASEK GEDE SEGARA CAYMAN <45864852+IwayanPasek@users.noreply.github.com>
2025-08-01 19:20:23 +0800

    Update isi_harga.php

--- proses/isi_harga.php
+++ proses/isi_harga.php
@@ -1,19 +1,14 @@
 <?php
 include('../config/db.php');
 
-// Mengambil data dari form
 $id = $_POST['id'];
 $harga = $_POST['harga'];
 
 try {
-    // Menyiapkan query UPDATE dengan placeholder
     $sql = "UPDATE pelanggan SET harga = ? WHERE id = ?";
     $stmt = $conn->prepare($sql);
-    
-    // Menjalankan query dengan data yang sesuai
     $stmt->execute([$harga, $id]);
 
-    // Mengalihkan kembali ke dashboard
     header('Location: ../dashboard.php');
 } catch (PDOException $e) {
     die("Error memperbarui harga: " . $e->getMessage());
